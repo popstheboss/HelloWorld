a5b42232e94822703fbe342ad5b3a9d1fd8127c0
Antonio Forrest <antonio.forrest.2023@student.lhschools.org>
2020-02-24 13:18:04 -0500

    intellij commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+            System.out.println("Hello World!");
+            System.out.print("My name is");
+            System.out.println(" joe mama");
+            System.out.println("This summer I when to kings island.");
+            System.out.println("I also played a lot of video games.");
+            System.out.println("lastly, I went on a trip to a few other states.");
+        }
+    }
+
+
